c121cd4966cf4f3946fa914801cd74f5ff17a511
durok <durok@firemail.cc>
2020-01-04 19:23:33 +0100

    source_code_test: test script for huffman code source_coding: code words of huffman tree are now sorted by occurance source_decoding: huffman decoder

--- src/receiver/source_decoding.m
+++ src/receiver/source_decoding.m
@@ -1,13 +1,26 @@
 function u_hat = source_decoding(b_hat_buf, code_tree, len_idx, switch_off)
 
-for blocks = 1:1%length(len_idx)
+tmp_out = [];
+% divide input stream in blocks of length len_idx
+for blocks = 1:length(len_idx)
     tmp_block = b_hat_buf(sum(len_idx(1:blocks-1))+1:sum(len_idx(1:blocks)));
-    tmp_code_tree = code_tree{blocks};
-    
-    
-    
+    tmp_sym = code_tree{blocks}.sym_list;
+    tmp_cw = code_tree{blocks}.cw;
+
+    % find code words in stream and remove found cw, add corresponding bit
+    % vector to output 
+    % do until block is empty
+    while (~isempty(tmp_block))
+        for i = 1:length(tmp_cw)
+            if (ismember(tmp_block(1:length(tmp_cw{i}))',tmp_cw{i},'rows'))
+                tmp_block = tmp_block(length(tmp_cw{i})+1:end);
+                tmp_out = [tmp_out; tmp_sym{i}];
+                break;
+                
+            error('source_decoding: bit combination not found');
+            end
+        end
+    end
 end
-
-
-
+u_hat = tmp_out;
 end
